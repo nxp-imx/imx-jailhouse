d36bd71b2a15e9b618155ae9c5803ec0688e606c
Flynn xu <flynn.xu@nxp.com>
2018-11-29 14:14:36 +0800

    imx8mq: non-root cell memory config confict
    old config put non-root cell start 0xffaf0000, but
    TEE was reside at 0xfe000000-0xffffffff, which will
    cause a conflict. Move the cell to 0xc0000000.
    
    Signed-off-by: Flynn xu <flynn.xu@nxp.com>
    (cherry picked from commit 25e14a5d05ecca13a7b9045dd1bf7cea2193e5d2)

--- configs/arm64/imx8mq-gic-demo.c
+++ configs/arm64/imx8mq-gic-demo.c
@@ -45,8 +45,8 @@ struct {
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
 		},
-		/* RAM: Top at 4GB Space */ {
-			.phys_start = 0xffaf0000,
+		/* RAM: Bottom at 4GB Space */ {
+			.phys_start = 0xc0000000,
 			.virt_start = 0,
 			.size = 0x00010000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
