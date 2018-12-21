3d2a172b2d28047bdb6568b95e23aa1381c2c684
Flynn xu <flynn.xu@nxp.com>
2018-12-21 09:57:26 +0800

    smc: forward smc call to optee
    smc call was trigger in EL1, handled in jailhouse,
    forward it to optee.
    
    Signed-off-by: Flynn xu <flynn.xu@nxp.com>
    (cherry picked from commit 215da4b031cdd02f5b8f0cc15ad1722c517b1b39)

--- hypervisor/arch/arm-common/smccc.c
+++ hypervisor/arch/arm-common/smccc.c
@@ -2,9 +2,11 @@
  * Jailhouse, a Linux-based partitioning hypervisor
  *
  * Copyright (C) 2018 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright 2018 NXP
  *
  * Authors:
  *  Lokesh Vutla <lokeshvutla@ti.com>
+ *  Flynn Xu<flynn.xu@nxp.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
@@ -14,6 +16,30 @@
 #include <asm/psci.h>
 #include <asm/traps.h>
 #include <asm/smccc.h>
+
+static void trust_dispatch(struct trap_context *ctx)
+{
+	register unsigned long x0 asm("x0") = ctx->regs[0];
+	register unsigned long x1 asm("x1") = ctx->regs[1];
+	register unsigned long x2 asm("x2") = ctx->regs[2];
+	register unsigned long x3 asm("x3") = ctx->regs[3];
+	register unsigned long x4 asm("x4") = ctx->regs[4];
+	register unsigned long x5 asm("x5") = ctx->regs[5];
+	register unsigned long x6 asm("x6") = ctx->regs[6];
+	register unsigned long x7 asm("x7") = ctx->regs[7];
+
+	asm volatile ("smc #0\n"
+		      : "+r" (x0), "+r" (x1), "+r" (x2), "+r" (x3),
+		      "+r" (x4), "+r" (x5), "+r" (x6), "+r" (x7)
+		      :
+		      : "x8", "x9", "x10", "x11", "x12",
+		      "x13", "x14", "x15", "x16", "x17" );
+
+	ctx->regs[0] = x0;
+	ctx->regs[1] = x1;
+	ctx->regs[2] = x2;
+	ctx->regs[3] = x3;
+}
 
 static long handle_arch(struct trap_context *ctx)
 {
@@ -53,6 +79,11 @@ enum trap_return handle_smc(struct trap_context *ctx)
 
 	case ARM_SMCCC_OWNER_STANDARD:
 		regs[0] = psci_dispatch(ctx);
+		break;
+
+	case ARM_SMCCC_OWNER_TRUSTED_APP ... ARM_SMCCC_OWNER_TRUSTED_APP_END:
+	case ARM_SMCCC_OWNER_TRUSTED_OS ... ARM_SMCCC_OWNER_TRUSTED_OS_END:
+		trust_dispatch(ctx);
 		break;
 
 	default:
