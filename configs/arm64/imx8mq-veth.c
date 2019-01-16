32124a122b5850046a569bd9dd0492be89f7208b
Flynn xu <flynn.xu@nxp.com>
2019-01-15 20:11:45 -0500

    arch64: configs: fix imx8mq-veth console flag
    fix imx8mq-veth console flag and type
    
    Signed-off-by: Flynn xu <flynn.xu@nxp.com>
    (cherry picked from commit d15f1c900e12771182406f3a23609a56d8160b4d)

--- configs/arm64/imx8mq-veth.c
+++ configs/arm64/imx8mq-veth.c
@@ -27,6 +27,7 @@ struct {
 	.header = {
 		.signature = JAILHOUSE_SYSTEM_SIGNATURE,
 		.revision = JAILHOUSE_CONFIG_REVISION,
+		.flags = JAILHOUSE_SYS_VIRTUAL_DEBUG_CONSOLE,
 		.hypervisor_memory = {
 			.phys_start = 0xffc00000,
 			.size =       0x00400000,
@@ -34,10 +35,10 @@ struct {
 		.debug_console = {
 			.address = 0x30860000,
 			.size = 0x1000,
-			.flags = JAILHOUSE_CON1_TYPE_IMX |
-				 JAILHOUSE_CON1_ACCESS_MMIO |
-				 JAILHOUSE_CON1_REGDIST_4 |
-				 JAILHOUSE_CON2_TYPE_ROOTPAGE,
+			.flags = JAILHOUSE_CON_TYPE_IMX |
+				 JAILHOUSE_CON_ACCESS_MMIO |
+				 JAILHOUSE_CON_REGDIST_4,
+			.type = JAILHOUSE_CON_TYPE_IMX,
 		},
 		.platform_info = {
 			/*
