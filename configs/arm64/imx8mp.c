c2104440c7783e03ea20adee586851879512bc25
Alice Guo <alice.guo@nxp.com>
2021-02-18 16:38:59 +0800

    MLK-25305 configs: imx8m: add additional memory region for IO
    It is necessary to give root cell access to the region between 3890_0000
    and 4000_0000. See Cortex-A53 Memory Map in Reference Manual.
    
    Signed-off-by: Alice Guo <alice.guo@nxp.com>

--- configs/arm64/imx8mp.c
+++ configs/arm64/imx8mp.c
@@ -18,7 +18,7 @@
 struct {
 	struct jailhouse_system header;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[23];
+	struct jailhouse_memory mem_regions[24];
 	struct jailhouse_irqchip irqchips[3];
 	struct jailhouse_pci_device pci_devices[4];
 } __attribute__((packed)) config = {
@@ -133,6 +133,13 @@ struct {
 			.phys_start = 0x00000000,
 			.virt_start = 0x00000000,
 			.size =	      0x38800000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* IO */ {
+			.phys_start = 0x38900000,
+			.virt_start = 0x38900000,
+			.size =	      0x7700000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
 		},
